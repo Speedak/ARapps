e5c98d5d1c67e3ce6601ef6fdfed15a5097b9268
ALeonov <aleonov92@gmail.com>
2019-08-11 23:57:58 +0300

    New App AR

---
+++ ArApp/Assets/AnimationManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    private Animation _animation;
+    private bool _IsInAction = false;
+    // Start is called before the first frame update
+    void Start(){
+     _animation = GetComponent<Animation>();  
+    }
+     
+    public void Walk()
+    {
+    	if(_IsInAction==true)
+    	return;
+
+    	_IsInAction = true;
+    	_animation.play("Spider_Armature|run_ani_vor");
+    	Invoke("ResetAnimation", 0.14f);
+    }
+
+    public void Jump()
+    {
+    	if(_IsInAction==true)
+    	return;
+
+    	_IsInAction = true;
+    	_animation.play("Spider_Armature|Jump");
+    	Invoke("ResetAnimation", 1.4f);
+    }
+
+
+    private void ResetAnimation()
+    {
+    	_animation.play("idle");
+    	_IsInAction == false;
+    } 
+
+
+}
