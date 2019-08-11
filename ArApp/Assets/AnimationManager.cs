60c26d9418ce08a72d93e418264004760b804ab4
ALeonov <aleonov92@gmail.com>
2019-08-12 00:00:15 +0300

    New AR App

--- ArApp/Assets/AnimationManager.cs
+++ ArApp/Assets/AnimationManager.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    private Animation _animation;
-    private bool _IsInAction = false;
+    private Animation  _animation;
+    private bool  _IsInAction = false;
     // Start is called before the first frame update
-    void Start(){
-     _animation = GetComponent<Animation>();  
-    }
+     void Start(){
+      _animation = GetComponent<Animation>();  
+     }
      
-    public void Walk()
-    {
-    	if(_IsInAction==true)
-    	return;
+     public void Walk()
+     {
+        if(_IsInAction==true)
+         return;
 
-    	_IsInAction = true;
-    	_animation.play("Spider_Armature|run_ani_vor");
-    	Invoke("ResetAnimation", 0.14f);
-    }
+     _IsInAction = true;
+     _animation.Play("Spider_Armature|run_ani_vor");
+     Invoke("ResetAnimation", 0.14f);
+     }
 
-    public void Jump()
-    {
-    	if(_IsInAction==true)
-    	return;
-
-    	_IsInAction = true;
-    	_animation.play("Spider_Armature|Jump");
-    	Invoke("ResetAnimation", 1.4f);
-    }
+    
+    //Spider_Armature|normal
+    //public void Jump()
+    //{
+    //if(_IsInAction==true)
+    // return;
+    //
+    // _IsInAction = true;
+    //_animation.play("Spider_Armature|Jump");
+    //Invoke("ResetAnimation", 1.4f);
+    //}
 
 
     private void ResetAnimation()
     {
-    	_animation.play("idle");
-    	_IsInAction == false;
+    _animation.Play("idle");
+    _IsInAction = false;
     } 
 
 
